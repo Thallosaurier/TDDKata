9a3fa83dcd7bc03ec7bfad0d9715529543efa7ae
knapp <knapp@192.168.192.53>
2017-04-23 16:11:46 +0200

    added more numbertests

--- TDDKata/src/test/java/machine/ArabicToRomanNumbersConverterTest.java
+++ TDDKata/src/test/java/machine/ArabicToRomanNumbersConverterTest.java
@@ -12,12 +12,22 @@ public class ArabicToRomanNumbersConverterTest {
 	private ArabicToRomanNumbersConverter arabicToRomanNumbersConverter = new ArabicToRomanNumbersConverter();
 
 	@Test
+	public void testArabicNumberOneToRomanNumberConversion() throws Exception {
+		Assert.assertEquals("I", arabicToRomanNumbersConverter.convert(1));
+	}
+
+	@Test
+	public void testArabicNumberTenToRomanNumberConversion() throws Exception {
+		Assert.assertEquals("X", arabicToRomanNumbersConverter.convert(10));
+	}
+
+	@Test
 	public void testArabicNumberElevenToRomanNumberConversion() throws Exception {
 		Assert.assertEquals("XI", arabicToRomanNumbersConverter.convert(11));
 	}
 
 	@Test
 	public void testArabicNumberTwelveToRomanNumberConversion() throws Exception {
-		Assert.assertEquals("XII", arabicToRomanNumbersConverter.convert(11));
+		Assert.assertEquals("XII", arabicToRomanNumbersConverter.convert(12));
 	}
 }
